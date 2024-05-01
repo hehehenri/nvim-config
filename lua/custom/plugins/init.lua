005fc54a7a4d661b48340231f07adfa6e998f6ff
henri <henri@henr.in>
2024-05-01 16:40:43 -0300

    feat: add hop

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,1 +1,13 @@
-return {}
+return {
+  { -- Jump anywhere in a document file.
+    'smoka7/hop.nvim',
+    lazy = false,
+    version = '*',
+    config = function()
+      local hop = require 'hop'
+      hop.setup()
+
+      vim.keymap.set('', 'F', hop.hint_words)
+    end,
+  },
+}
